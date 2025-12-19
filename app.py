19d5606cc87b5f9638a8153302d19837262baefa
Ongeri123 <ongerinewton10@gmail.com>
2025-12-19 05:12:48 +0300

    updated

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify
+from flask_migrate import Migrate
+from models import db
 
 app = Flask(__name__)
 
-@app.route('/', methods= ['GET'])
-def home():
-    return jsonify({'message' : 'Hello, World!'})
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+
+# initialize the SQLAlchemy instance defined in `models.py`
+db.init_app(app)
+# create a migration object to manage migrations
+migrate = Migrate(app, db)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
